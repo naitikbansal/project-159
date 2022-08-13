b586aa48256233ff83f127e1cf5c2ec1516e3af8
naitikbansal <73417177+naitikbansal@users.noreply.github.com>
2022-08-13 11:27:45 +0530

    Add files via upload

---
+++ js/infoBanner.js
@@ -0,0 +1,108 @@
+AFRAME.registerComponent("info-banner", {
+  schema: {
+    itemId: { default: "", type: "string" },
+  },
+  update: function () {
+    this.createBanner();
+  },
+  createBanner: function () {
+    postersInfo = {
+      superman: {
+        banner_url: "./assets/posters/superman-banner.jpg",
+        title: "Superman",
+        released_year: "1983",
+        description:"Superman is an interesting comic.You should read it."
+      },
+      spiderman: {
+        banner_url: "./assets/posters/spiderman-banner.png",
+        title: "Spiderman",
+        released_year: "1962",
+        description:"Spiderman is an action comic.It will surely be a source of entertainment for you"
+      },
+      "captain-aero": {
+        banner_url: "./assets/posters/captain-aero-banner.jpg",
+        title: "Captain Aero",
+        released_year: "1942",
+        description:"Read the story of captain aero and enjoy!"
+      },
+      "outer-space": {
+        banner_url: "./assets/posters/outer-space-banner.jpg",
+        title: "Outer Space",
+        released_year: "1952",
+        description:
+          "See the outer space!",
+      },
+    };
+    const { itemId } = this.data;
+
+    const fadeBackgroundEl = document.querySelector("#fadeBackground");
+
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("id", `${itemId}-banner`);
+
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.9,
+      height: 1,
+    });
+
+    entityEl.setAttribute("material", { color: "#000" });
+    entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+
+    const item = postersInfo[itemId];
+
+    const imageEl = this.createImageEl(item);
+    const titleEl = this.createTitleEl(item);
+    const descriptionEl = this.createDescriptionEl(item);
+
+    entityEl.appendChild(imageEl);
+    entityEl.appendChild(titleEl);
+    entityEl.appendChild(descriptionEl);
+
+    fadeBackgroundEl.appendChild(entityEl);
+  },
+  createImageEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.85,
+      height: 0.4,
+    });
+    entityEl.setAttribute("material", { src: item.banner_url });
+    entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+    return entityEl;
+  },
+  createTitleEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 1.2,
+      height: 2,
+      color: "#fff",
+      value: `${item.title} (${item.released_year})`,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+    return entityEl;
+  },
+  createDescriptionEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 0.75,
+      height: 2,
+      color: "#fff",
+      wrapCount: "40",
+      value: item.description,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+    return entityEl;
+  },
+});
